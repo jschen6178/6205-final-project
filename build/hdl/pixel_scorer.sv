19a14532517c9b226d31953c644f7cab2729187d
dlee888 <dlee888@mit.edu>
2024-12-07 20:45:54 -0500

    finish pixel scorer

--- build/hdl/pixel_scorer.sv
+++ build/hdl/pixel_scorer.sv
@@ -39,20 +39,56 @@ module pixel_scorer #(
   logic pixel_in_pipe[0:1];
   logic [DWIDTH-1:0] prev_valid_pixel_distance;
 
+  logic doing_backward_pass;
+  logic [HWIDTH-1:0] backward_hcount;
+  logic [VWIDTH-1:0] backward_vcount;
+  logic [HWIDTH-1:0] backward_hcount_pipe[0:1];
+  logic [VWIDTH-1:0] backward_vcount_pipe[0:1];
+  logic backward_valid_pipe[0:1];
+  logic [1:0] line_buff_weebs;
+  logic [DWIDTH-1:0] backward_line_buff_data[0:1];
+  logic [DWIDTH-1:0] prev_backward_pixel_distance;
+
+  logic [DWIDTH-1:0] real_read_data, real_prev_pixel_distance;
+  logic [DWIDTH-1:0] real_line_buff_data, real_backward_prev_pixel_distance;
   always_comb begin
     if (pixel_valid_in) begin
       read_addr = (pixel_vcount_in - 1) * HRES + pixel_hcount_in;
+    end else if (doing_backward_pass) begin
+      read_addr = backward_vcount * HRES + backward_hcount;
     end else begin
       read_addr = vcount_in * HRES + hcount_in;
     end
     if (input_data_valid_pipe[1]) begin
       write_addr = input_vcount_pipe[1] * HRES + input_hcount_pipe[1];
+      real_read_data = input_vcount_pipe[1] == 0 ? INF : read_data;
+      real_prev_pixel_distance = input_hcount_pipe[1] == 0 ? INF : prev_valid_pixel_distance;
       if (pixel_in_pipe[1]) begin
         write_data = 0;
-      end else if (prev_valid_pixel_distance == INF && read_data == INF) begin
+      end else if (real_prev_pixel_distance == INF && real_read_data == INF) begin
         write_data = INF;
       end else begin
-        write_data = prev_valid_pixel_distance < read_data ? prev_valid_pixel_distance + 1 : read_data + 1;
+        write_data = real_prev_pixel_distance < real_read_data ? real_prev_pixel_distance + 1 : real_read_data + 1;
+      end
+      write_enable = 1;
+    end else if (backward_valid_pipe[1]) begin
+      write_addr = backward_vcount_pipe[1] * HRES + backward_hcount_pipe[1];
+      if (backward_vcount_pipe[1] == VRES - 1) begin
+        real_line_buff_data = INF;
+      end else if (line_buff_weebs == 2'b01) begin
+        real_line_buff_data = backward_line_buff_data[0];
+      end else begin
+        real_line_buff_data = backward_line_buff_data[1];
+      end
+      real_backward_prev_pixel_distance = backward_hcount_pipe[1] == HRES - 1 ? INF : prev_backward_pixel_distance;
+      if (read_data == INF && real_line_buff_data == INF && real_backward_prev_pixel_distance == INF) begin
+        write_data = INF;
+      end else if (read_data < real_line_buff_data && read_data < real_backward_prev_pixel_distance) begin
+        write_data = read_data + 1;
+      end else if (real_line_buff_data < read_data && real_line_buff_data < real_backward_prev_pixel_distance) begin
+        write_data = real_line_buff_data + 1;
+      end else begin
+        write_data = real_backward_prev_pixel_distance + 1;
       end
       write_enable = 1;
     end else begin
@@ -65,8 +101,10 @@ module pixel_scorer #(
       output_data_valid_pipe[0] <= 0;
       output_data_valid_pipe[1] <= 0;
       prev_valid_pixel_distance <= INF;
-      input_data_valid_pipe[0]  <= 0;
-      input_data_valid_pipe[1]  <= 0;
+      input_data_valid_pipe[0] <= 0;
+      input_data_valid_pipe[1] <= 0;
+      backward_valid_pipe[0] <= 0;
+      backward_valid_pipe[1] <= 0;
     end else begin
       // Logic for when we're not inputting a new skeleton
       output_data_valid_pipe[0] <= !pixel_valid_in;
@@ -90,6 +128,42 @@ module pixel_scorer #(
       input_data_valid_pipe[1] <= input_data_valid_pipe[0];
       if (input_data_valid_pipe[1]) begin
         prev_valid_pixel_distance <= write_data;
+      end
+      // When we're done with the forward pass, start the backward pass
+      if (input_data_valid_pipe[1] && input_hcount_pipe[1] == HRES - 1 && input_vcount_pipe[1] == VRES - 1) begin
+        doing_backward_pass <= 1;
+        backward_hcount <= HRES - 1;
+        backward_vcount <= VRES - 1;
+        line_buff_weebs <= 2'b01;
+      end
+      // Update hcount and vcount for the backward pass
+      if (doing_backward_pass) begin
+        if (backward_hcount == 0) begin
+          if (backward_vcount == 0) begin
+            doing_backward_pass <= 0;
+            backward_valid_pipe[0] <= 0;
+          end else begin
+            backward_vcount <= backward_vcount - 1;
+            backward_hcount <= HRES - 1;
+            backward_valid_pipe[0] <= 1;
+            line_buff_weebs <= {line_buff_weebs[0], line_buff_weebs[1]};
+          end
+        end else begin
+          backward_hcount <= backward_hcount - 1;
+          backward_valid_pipe[0] <= 1;
+        end
+      end
+      backward_hcount_pipe[0] <= backward_hcount;
+      backward_hcount_pipe[1] <= backward_hcount_pipe[0];
+      backward_vcount_pipe[0] <= backward_vcount;
+      backward_vcount_pipe[1] <= backward_vcount_pipe[0];
+      backward_valid_pipe[1]  <= backward_valid_pipe[0];
+      if (backward_valid_pipe[1]) begin
+        if (line_buff_weebs == 2'b01) begin
+          prev_backward_pixel_distance <= backward_line_buff_data[0];
+        end else begin
+          prev_backward_pixel_distance <= backward_line_buff_data[1];
+        end
       end
     end
   end
@@ -115,24 +189,29 @@ module pixel_scorer #(
       .doutb(read_data)
   );
 
-  xilinx_true_dual_port_read_first_1_clock_ram #(
-      .RAM_WIDTH(DWIDTH),
-      .RAM_DEPTH(HRES)
-  ) line_buffer (
-      .addra(write_addr),
-      .addrb(read_addr),
-      .dina(write_data),
-      .dinb(),
-      .clka(clk_in),
-      .wea(write_enable),
-      .web(1'b0),
-      .ena(1'b1),
-      .enb(1'b1),
-      .rsta(rst_in),
-      .rstb(rst_in),
-      .regcea(1'b1),
-      .regceb(1'b1),
-      .douta(),
-      .doutb(read_data)
-  );
+  generate
+    genvar i;
+    for (i = 0; i < 2; i++) begin
+      xilinx_true_dual_port_read_first_1_clock_ram #(
+          .RAM_WIDTH(DWIDTH),
+          .RAM_DEPTH(HRES)
+      ) line_buffer (
+          .addra(backward_hcount_pipe[1]),
+          .addrb(backward_hcount),
+          .dina(write_data),
+          .dinb(),
+          .clka(clk_in),
+          .wea(line_buff_weebs[i] && backward_valid_pipe[1]),
+          .web(1'b0),
+          .ena(1'b1),
+          .enb(1'b1),
+          .rsta(rst_in),
+          .rstb(rst_in),
+          .regcea(1'b1),
+          .regceb(1'b1),
+          .douta(),
+          .doutb(backward_line_buff_data[i])
+      );
+    end
+  endgenerate
 endmodule
